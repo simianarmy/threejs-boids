ba4a838366975279fcc3fe78776a206fa24d824a
Marc Mauger <simianarmy@gmail.com>
2018-12-29 14:43:38 -0500

    increased bounds constant:

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,5 @@
-export const WIDTH = 20; // Controls total # of boids
+export const WIDTH = 20; // Controls total # of boids and size of texture maps
 export const NUM_BIRDS = WIDTH * WIDTH;
-export const BOUNDS = 2000;
+export const BOUNDS = 10000; // how does this relate to world_width?
 export const BOUNDS_HALF = BOUNDS / 2;
 export const WORLD_WIDTH = 3000;
